32107fc0b9e04e3147828f43416e2c848e05c983
Eric Denton <eric@iamericanartist.com>
2016-07-04 17:45:30 -0500

    finished exercise, updated README, added screenshot and Codepen link

--- js/main.js
+++ js/main.js
@@ -1,63 +1,81 @@
 "use strict";
-console.log("main.js loaded");
+// console.log("main.js loaded");
 
-/*Create a function named Car with one property named `manufactured_date`
-  whose value will be Date.now()*/
-function Car () {
-  this.tires = [];
-  // Create the manufactured date property (see above)
-  this.manufactured_date = Date();  /*The Date.now() method returns the 
-      number of milliseconds elapsed since 1 January 1970 00:00:00 UTC. 
-      Date() returns human legible format.*/
+function Car () {                         // Create a function named Car with one property named `manufactured_date` whose value will be Date.now()
+  this.manufactured_date = Date();        // Create the manufactured date property (see above)
+  this.tint = "black";
 }
 
-/*Create a constructor function named Make with one 
-  argument that will hold the manufacturer of the Make (see below). 
-
-  Set its prototype to a new instance of the Car function.
-
-  Define a property `manufacturer` and assign the 
-  manufacturer property to the argument value.*/
-function Make (maker) {
-  this.manufacturer = maker;
+function Make (maker) {                   // Create a constructor function named Make with one argument that will hold the manufacturer of the Make (see below). 
+  this.manufacturer = maker;              // Define a property `manufacturer` and assign the manufacturer property to the argument value.
 }
-// Set the prototype to Car
-Make.prototype = new Car();
+Make.prototype = new Car();               // Set its prototype to a new instance of the Car function.
 
 
-// ****************************************************** 
-// 1. Create a new constructor function for the make of your favorite car.  Example:
-    // function YourFavoriteMake (name) {
-    //   this.modelName = name;
-    // }
-// 2. It must inherit from the general Make function.
-    // YourFavoriteMake.prototype = new Make(...);
-// 3. It must have one argument that will to hold the model name.
-// 4. The constructor must define a property to hold the model name.
-function Kia (name) {
-  this.modelName = name;
-}
-Kia.prototype = new Make("Kia");
-
-
-function LicensePlate(num){
+function LicensePlate(num, state){        // This is a CONSTRUCTOR FUNCTION for each cars license 
   this.number = num;
-  this.state = "TN";
+  this.state = state;
 }
 
-function Sorento (plateNum) {
-  this.plateNum = null;
-  this.plate = new LicensePlate(plateNum);
-}
-// Set the prototype to appropriate model you created above and set the model name argument
-Sorento.prototype = new Kia("Sorento");
 
-var mySuv = new Sorento("345-KYP");
+
+//////////////////////First Car
+function Kia (name) {
+  this.modelName = name;                  // 4. The constructor must define a property to hold the model name.
+}
+Kia.prototype = new Make("Kia");          // 2. It must inherit from the general Make function.     // YourFavoriteMake.prototype = new Make(...);    // 3. It must have one argument that will to hold the model name.
+
+
+function Sorento (plateNum, state) {            // This is a Sorento CONSTRUCTOR FUNCTION
+  this.tires = [];
+  this.plate = new LicensePlate(plateNum, state);
+}
+Sorento.prototype = new Kia("Sorento LX");      // Set the prototype to appropriate model you created above and set the model name argument
+
+var mySuv = new Sorento("345-KYP", "MN");
 console.log("mySuv", mySuv);
 
 
-// ****************************************************** 
-//Tires prototype chain
+
+//////////////////////Second Car
+function Honda (name) {
+  this.modelName = name;                      // 4. The constructor must define a property to hold the model name.
+}
+Honda.prototype = new Make("Honda");
+
+
+function Accord (plateNum, state) {           // This is a Accord CONSTRUCTOR FUNCTION
+  this.tires = [];
+  this.seat = "buckets";
+  this.plate = new LicensePlate(plateNum, state);
+}
+Accord.prototype = new Honda("Accord LX");    // Set the prototype to appropriate model you created above and set the model name argument
+
+var EricsHonda = new Accord("asdf", "FL");
+console.log("EricsHonda",EricsHonda);
+
+
+
+//////////////////////Third Car
+function Chevy (name) {
+  this.modelName = name;                      // 4. The constructor must define a property to hold the model name.
+}
+Chevy.prototype = new Make("Chevy");
+
+
+function Nova (plateNum, state) {             // This is a Nova CONSTRUCTOR FUNCTION
+  this.tires = [];
+  this.bodystyle = "Hatchback";
+  this.plate = new LicensePlate(plateNum, state);
+}
+Nova.prototype = new Chevy("Nova");           // Set the prototype to appropriate model you created above and set the model name argument
+
+var MomsChevy = new Nova("PaidFor", "TN");
+console.log("MomsChevy",MomsChevy);
+
+
+
+//////////////////////Tires Prototype Chain
 function Tire() {
   this.shape = "round";
 }
@@ -85,22 +103,26 @@ var racing2 = new tireType("racing");
 var snowTire1 = new tireType("snow");
 var snowTire2 = new tireType("snow");
 
-mySuv.tires.push(racing1, racing2, snowTire1,snowTire2);
+mySuv.tires.push(racing1, racing2, snowTire1, snowTire2);
+MomsChevy.tires.push(racing2, racing2, racing1, racing1);
+EricsHonda.tires.push(racing1, racing2, snowTire1, snowTire2);
 
+for (var i = 0; i < EricsHonda.tires.length; i++) {
+  EricsHonda.tires[i].setPSI(59);
+  EricsHonda.tires[i].setRadius(35);
+}
 for (var i = 0; i < mySuv.tires.length; i++) {
   mySuv.tires[i].setPSI(32);
   mySuv.tires[i].setRadius(20);
 }
-console.log("These here tires are ", mySuv.tires);
-// console.log("mySuv ", mySuv);
+for (var i = 0; i < MomsChevy.tires.length; i++) {
+  MomsChevy.tires[i].setPSI(30);
+  MomsChevy.tires[i].setRadius(30);
+}
+
+console.log("mySuv tires are ", mySuv.tires[3]);
+console.log("EricsHonda tires are ", EricsHonda.tires[3]);
+console.log("MomsChevy tires are ", MomsChevy.tires[3]);
 // console.log("mySuv tire #3 ", mySuv.tires[2]);
-// console.log("mySuv.tires[0].PSI ",mySuv.tires[0].PSI);
 // mySuv.tires[0].PSI = 22;
-
-// ******************************************************
-/*Being an avid car collector, you own three different cars all of
-    same model from your favorite manufacturer.
-
-    1. Create three new instances of your favorite car.
-    2. Create a new property on each object to hold the license plate
-       number for each car.*/
+// console.log("mySuv.tires[0].PSI ",mySuv.tires[0].PSI);
